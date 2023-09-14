6ab9765b85d141677c77e0ecbf59359d5191a98f
Oussama Haddi <oussama.turboxxx1@gmail.com>
2023-09-14 17:24:37 +0100

    create uml and classes

---
+++ src/Phase.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+import java.util.List;
+
+public class Phase {
+    private String code;
+    private String title;
+    private String description;
+    private Date dateStart;
+    private Date dateEnd;
+    private String amount;
+    private boolean statue;
+    private boolean billed;
+    private boolean payed;
+
+   private List<Project> projects;
+}
---
+++ src/Project.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+import java.util.List;
+
+public class Project {
+    private String code;
+    private String name;
+    private String description;
+    private Date dateStart;
+    private Date dateEnd;
+    private String status;
+    private Double amount;
+
+    private List<Phase> phases;
+
+}
